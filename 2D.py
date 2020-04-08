e317a6640dac0caaa340fe8aa3545d5a4fc5cc5f
Aradhana <aradhana.singh@team.telstra.com>
2020-04-09 02:20:58 +0530

    2D array example

---
+++ 2D.py
@@ -0,0 +1,18 @@
+#REcatangular array
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+# # matrix[0][1] = 20
+# # print(matrix[0][1])
+# for row in matrix:
+#     for item in row:
+#         print(item)
+numbers = [2, 5, 4, 7, 5, 9]
+# # numbers.insert(0,10) # add item
+# numbers.remove(2)  # Remove index clear()
+# numbers.pop(1)
+print(50 in numbers)
+print(numbers.count(5))
+print(numbers)
