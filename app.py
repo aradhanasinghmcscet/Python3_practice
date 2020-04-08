c5659c3a21a7f1a2c33de1b457d9859fba6b42e4
Aradhana <aradhana.singh@team.telstra.com>
2020-04-09 01:59:28 +0530

    some more examples

--- app.py
+++ app.py
@@ -26,7 +26,35 @@
 #         break
 #     else:
 #         print("Sorry, I don't understand")
-for item in "Python":
-    print(item)
-for arr in ["Aradhana", "Singh"]:
-    print(arr)
+# for item in "Python":
+#     print(item)
+# for arr in ["Aradhana", "Singh"]:
+#     print(arr)
+
+# for item in range(5, 10, 2):  # 2 steps forward
+#     print(item)
+# prices = [10, 20, 30]
+# total = 0
+# for price in prices:
+#     total +=  price
+# print(f"total: {total}")
+# Nested loop (x,y), (0,0).....
+# for x in range(4):
+#     for y in range(3):
+# #         print(f'({x}, {y})')
+# numbers = [5, 2, 5, 2, 2]
+# for item in numbers:
+#     # print('x'* item)
+#     output = ''
+#     for count in range(item):
+#         output += 'x'
+#     print(output)
+# names = ["Aradhana", "Anu", "Rahul"]
+# names[0] = "Neha"
+# print(names[0:]) # 1:3 , -1 -2 , :2 etc
+numbers = [3, 6, 2, 8, 4, 10, 13]
+max = numbers[0]
+for number in numbers:
+    if number > max:
+        max = number
+print(max)
